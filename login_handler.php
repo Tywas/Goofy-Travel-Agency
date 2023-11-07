8465016049103634417b8928a6940d19194f7f0d
Tyler Bowes <tylerbowes@u.boisestate.edu>
2023-11-06 17:51:48 -0700

    login authentcation

--- login_handler.php
+++ login_handler.php
@@ -13,17 +13,54 @@ $logger->LogDebug("User [{$username}] attempting to log in");
 //TODO:  Validate username and password
 // for now check if exact name and password
 
-if ($username == 'tywas' && $password == 'abc123') {
-    $_SESSION['authenticated'] = true;
-    header("Location: user.php");
-    exit();
-} else {
-    $logger->LogWarn("User [{$username}] invalid username or password");
-    $_SESSION['authenticated'] = false;
-    $_SESSION['message'] = 'Invalid Username or Password';
-    // header("Location: login.php");
-    // exit();
+try {
+    $url = getenv('JAWSDB_URL');
+    $dbparts = parse_url($url);
+    $hostname = $dbparts['host'];
+    $dbusername = $dbparts['user'];
+    $dbpassword = $dbparts['pass'];
+    $database = ltrim($dbparts['path'],'/');
+    $conn = new PDO("mysql:host=$hostname;dbname=$database", $dbusername, $dbpassword);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+
+    $query = "SELECT user_id, Fname, username, pwd FROM users WHERE username=:username && pwd=:pwd;";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(":username", $username);
+    $stmt->bindParam(":pwd", $pwd);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if ($result) {
+        $row = $result->fetch_assoc();
+        if(isset($row)){
+            echo $row['user_id'];
+            echo $row['Fname'];
+            echo $row['username'];
+            echo $row['pwd'];
+        }
+    } else {
+        print_r($result,1);
+        echo "i dont think anything was taken from the database";
+    //call error recovery routine
+    }
 }
+catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    // die("Connection failed: " . $e->getMessage());
+}
+
+// if ($username == 'tywas' && $password == 'abc123') {
+//     $_SESSION['authenticated'] = true;
+//     header("Location: user.php");
+//     exit();
+// } else {
+//     $logger->LogWarn("User [{$username}] invalid username or password");
+//     $_SESSION['authenticated'] = false;
+//     $_SESSION['message'] = 'Invalid Username or Password';
+//     // header("Location: login.php");
+//     // exit();
+// }
 
 
 
