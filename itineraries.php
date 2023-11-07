fb4646781987011a55607e0c52f5c821790bc05f
Tyler Bowes <tylerbowes@u.boisestate.edu>
2023-11-07 15:52:07 -0700

    itinerary update

--- itineraries.php
+++ itineraries.php
@@ -20,59 +20,56 @@ $thisPage="itineraries";
             <? include "site_header.php" ?>
         </header>
 
-        <h1>Itinerary Insiration Page</h1>
+        <section id="itineraries-info">
+
+            <div class="itinerary-container">
+                <h1>Itinerary Insiration Page</h1>
+            
+                <a href="index.php" class="itinerary_link"> 
+                    <div class="itineraries"> 
+                        <h3 class="itinerary_title"> Puddle Jumping </h3>
+                        <img class="itinerary_img" src="Ininerary_images\Puddle.jpg"> </img>
+                    </div>
+                </a>
+
+                <a href="index.php" class="itinerary_link">
+                    <div class="itineraries"> 
+                        <h3 class="itinerary_title"> Space </h3>
+                        <img class="itinerary_img" src="Ininerary_images\Space.webp"> </img>
+
+                    </div>
+                </a>
+                
+                <a href="index.php" class="itinerary_link">
+                    <div class="itineraries"> 
+                        <h3 class="itinerary_title"> Rome (REMOTE) </h3>
+                        <img class="itinerary_img" src="Ininerary_images\Remote france.webp"> </img>
+                    </div>
+                </a>
+
+                <a href="index.php" class="itinerary_link">
+                    <div class="itineraries"> 
+                        <h3 class="itinerary_title"> Cave Exploring </h3>
+                        <img class="itinerary_img" src="Ininerary_images\Cave.jfif"> </img>
+                    </div>
+                </a>
+
+                <a href="index.php" class="itinerary_link">
+                    <div class="itineraries"> 
+                        <h3 class="itinerary_title"> Office Cubical </h3>
+                        <img class="itinerary_img" src="Ininerary_images\Office cubical.jfif"> </img>
+                    </div>
+                </a>
+
+                <a href="index.php" class="itinerary_link">
+                    <div class="itineraries"> 
+                        <h3 class="itinerary_title"> Elephant Simulation </h3>
+                        <img class="itinerary_img" src="Ininerary_images\Elephants.jpg"> </img>
+                    </div>
+                </a>
+            </div>
+        </section>
         
-        <a href="index.php" class="itinerary_link"> 
-            <div class="itineraries"> 
-                <h3 class="itinerary_title"> Puddle Jumping </h3>
-                <img class="itinerary_img" src="Ininerary_images\Puddle.jpg"> </img>
-            </div>
-        </a>
-
-        <a href="index.php" class="itinerary_link">
-            <div class="itineraries"> 
-                <h3 class="itinerary_title"> Space </h3>
-                <img class="itinerary_img" src="Ininerary_images\Space.webp"> </img>
-
-            </div>
-        </a>
-        
-        <a href="index.php" class="itinerary_link">
-            <div class="itineraries"> 
-                <h3 class="itinerary_title"> Rome (REMOTE) </h3>
-                <img class="itinerary_img" src="Ininerary_images\Remote france.webp"> </img>
-            </div>
-        </a>
-
-        <a href="index.php" class="itinerary_link">
-            <div class="itineraries"> 
-                <h3 class="itinerary_title"> Cave Exploring </h3>
-                <img class="itinerary_img" src="Ininerary_images\Cave.jfif"> </img>
-            </div>
-        </a>
-
-        <a href="index.php" class="itinerary_link">
-            <div class="itineraries"> 
-                <h3 class="itinerary_title"> Office Cubical </h3>
-                <img class="itinerary_img" src="Ininerary_images\Office cubical.jfif"> </img>
-            </div>
-        </a>
-
-        <a href="index.php" class="itinerary_link">
-            <div class="itineraries"> 
-                <h3 class="itinerary_title"> Elephant Simulation </h3>
-                <img class="itinerary_img" src="Ininerary_images\Elephants.jpg"> </img>
-            </div>
-        </a>
-
-        <!--  -->
-
-
-
-
-
-
-
         <?php include "site_footer.php"; ?>
     </body>
 </html>
