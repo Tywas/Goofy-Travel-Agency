0bc1eca7060659772be490cbb5af04ebcb72de3b
Tyler Bowes <tylerbowes@u.boisestate.edu>
2023-11-07 11:18:08 -0700

    added some content to front page

--- index.php
+++ index.php
@@ -16,15 +16,18 @@ $thisPage="index";
         <? include "site_header.php" ?>
     </header>
 
-    <div class="image_background_and_content">
-        <!-- <img class="itinerary_img" src="Ininerary_images\Space.webp"> </img> -->
-        <!-- text-align:center;white-space:pre-wrap; -->
-        <div class="image_content">
-            <h1 class='image_styling'> The Future of Travel</h1>
-        <div>
-    </div>
+    
     <section id="home">
-        <!-- Add introductory content here -->
+        <div class="image_container">
+            <img class="home_img" src="Ininerary_images\Space.webp"> 
+<!-- text-align:center;white-space:pre-wrap; -->
+            <div class="overlay-text">
+                <h1 class='image_styling'> The Future of Travel</h1>
+            </div>
+
+            </img>
+        </div>    
+    <!-- Add introductory content here -->
     </section>
 
     <?php include "site_footer.php"; ?>
