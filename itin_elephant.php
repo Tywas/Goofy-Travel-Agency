cc60ac136b6f564565d8518c307d4e83056b14d0
Tyler Bowes <tylerbowes@u.boisestate.edu>
2023-11-09 21:59:01 -0700

    elephant info and min width style

--- itin_elephant.php
+++ itin_elephant.php
@@ -40,32 +40,29 @@ session_start();
             </tr>
             <tr class="itinerary-table_info">
                 <th>1</th>
-                <th>Morning: Arrive at the Puddle Paradise Resort, a whimsical destination known for its record-breaking puddles. Get geared up with waterproof boots and a playful attitude.</th>
-                <th>Afternoon: Start with a warm-up session, learning the art of puddle jumping from expert instructors. Practice different jumping styles and techniques to maximize the splash.</th>
-                <th>Evening: Join a puddle-themed dance party by the water's edge. Let loose and groove to the rhythm while getting splashed by synchronized puddle fountains.</th>
+                <th>Morning: Embark on a mind-bending journey to the "Elephant Oasis Simulation." Prepare for an extraordinary experience as you enter the virtual realm that will transform you into an elephant, complete with realistic sights, sounds, and sensations.</th>
+                <th>Afternoon: Meet your new elephant herd companions in the vast and digitally replicated savannah. Establish a sense of camaraderie as you engage in playful activities and form bonds with your fellow virtual pachyderms.</th>
+                <th>Evening: Take part in a simulated elephant training session, where you learn how to use your trunk and tusks effectively. Sharpen your skills in a safe and controlled environment, enhancing your understanding of the challenges faced by real elephants in the wild.</th>
             </tr>
             <tr class="itinerary-table_info">
                 <th>2</th>
-                <th>Morning: Embark on a puddle exploration adventure. Discover hidden puddle gems in the resort's lush surroundings. Take a guided tour with puddle experts who share fascinating facts about the science of puddles.</th>
-                <th>Afternoon: Compete in the Great Puddle Jumping Challenge. Navigate obstacle courses, master long-distance jumps, and showcase your unique jumping style. Earn points for creativity and enthusiasm.</th>
-                <th>Evening: Relax with a soothing puddle-side spa experience. Indulge in mud masks and puddle-inspired treatments to rejuvenate and refresh.</th>
+                <th>Morning: Start your day with a virtual sunrise as you graze with the herd on the digital plains. Experience the thrill of foraging for food, simulating the intricate social dynamics of an elephant pack.</th>
+                <th>Afternoon: Participate in a group mud bath to cool off and strengthen social bonds. Feel the virtual mud on your virtual skin as you enjoy the refreshing sensation, just like a real elephant in the wild.</th>
+                <th>Evening: Find your elephant love interest and take them on a nice long walk.  Become vulnerable with your elephant love interest and get emotionally attached until you can see yourself living the rest of your human life with this elephant.</th>
             </tr>
             <tr class="itinerary-table_info">
                 <th>3</th>
-                <th>Morning: Take a hot air balloon ride over the Puddle Paradise Resort. Marvel at the landscape dotted with shimmering puddles as you enjoy breathtaking views from above.</th>
-                <th>Afternoon: Attend a puddle art workshop. Create your own masterpiece by incorporating puddle splashes into a canvas. Embrace the spontaneous and colorful world of puddle art.</th>
-                <th>Evening: Join a puddle-themed feast. Indulge in delicious dishes inspired by water, rain, and puddles. Enjoy the culinary delights while surrounded by the enchanting ambiance of illuminated puddles.</th>
+                <th>Morning: Embark on a simulated migration with your herd. Traverse different terrains, from grasslands to forests, as you navigate the challenges of the virtual wild. Encounter other simulated wildlife, adding a touch of adventure to your elephant experience.</th>
+                <th>Afternoon: Engage in a virtual waterhole gathering. Interact with other digital elephants, exchanging trunk-to-trunk greetings and communicating through realistic elephant sounds. Immerse yourself in the simulation's authenticity as you quench your virtual thirst.</th>
+                <th>Evening: Things hit a road block in your elephant relationship from the previous day.  Your one true love has deciced to cheat on you with Elehunk the baddest elephant on the block.  You must sit and sob with your fellow elephant friends you've made even though they are only there because they pity you.</th>
             </tr>
             <tr class="itinerary-table_info">
                 <th>4</th>
-                <th>Morning: Challenge yourself with extreme puddle jumping. Head to the resort's puddle cliffs and experience the thrill of jumping from different heights into giant puddles. Safety is a priority, but the excitement is guaranteed.</th>
+                <th>Morning: Experience a virtual thunderstorm and revel in the excitement of playing in the simulated rain with your elephant companions. Feel the droplets on your virtual skin and enjoy the immersive sensation of a digital downpour.</th>
                 <th>Afternoon: Participate in a puddle photography contest. Capture the most dynamic and picturesque puddle jumps, splashes, and reflections. Share your photos with fellow puddle enthusiasts for a chance to win exciting prizes.</th>
-                <th>Evening: Attend the Puddle Carnival, a lively celebration with games, rides, and entertainment inspired by the joy of puddle jumping. Conclude the night with a grand fireworks display reflecting off the resort's largest puddle.</th>
+                <th>Evening: Conclude your unforgettable journey as a virtual elephant with a farewell feast. Gather with your herd for a virtual celebration, complete with digital treats and a sense of accomplishment. Bid farewell to your elephant companions, knowing that you've experienced the wild in a unique and immersive way.</th>
             </tr>
-            <tr class="itinerary-table_info">
-                <th>5</th>
-                <th>Morning: Bid farewell to Puddle Paradise with a group puddle jump, creating a spectacular and memorable splash finale. Receive a certificate of achievement and a Puddle Jumping Champion medal as a memento of your exhilarating puddle adventure.</th>
-        </table>
+            </table>
 
 
         <?php include "site_footer.php"; ?>
