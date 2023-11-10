092f592321779ff234f0546a71f71484c9472b2d
Tyler Bowes <tylerbowes@u.boisestate.edu>
2023-11-09 21:51:09 -0700

    more styles and information of office

--- itin_office.php
+++ itin_office.php
@@ -40,31 +40,31 @@ session_start();
             </tr>
             <tr class="itinerary-table_info">
                 <th>1</th>
-                <th>Morning: Arrive at the Puddle Paradise Resort, a whimsical destination known for its record-breaking puddles. Get geared up with waterproof boots and a playful attitude.</th>
-                <th>Afternoon: Start with a warm-up session, learning the art of puddle jumping from expert instructors. Practice different jumping styles and techniques to maximize the splash.</th>
-                <th>Evening: Join a puddle-themed dance party by the water's edge. Let loose and groove to the rhythm while getting splashed by synchronized puddle fountains.</th>
+                <th>Morning: Embark on a journey to the "Ultimate Cubicle Haven," a state-of-the-art office cubicle that promises to redefine your work experience. Feel the anticipation as you approach the entrance, adorned with a holographic welcome sign that greets you personally.</th>
+                <th>Afternoon: Go to work.</th>
+                <th>Evening: Work overtime.</th>
             </tr>
             <tr class="itinerary-table_info">
                 <th>2</th>
-                <th>Morning: Embark on a puddle exploration adventure. Discover hidden puddle gems in the resort's lush surroundings. Take a guided tour with puddle experts who share fascinating facts about the science of puddles.</th>
-                <th>Afternoon: Compete in the Great Puddle Jumping Challenge. Navigate obstacle courses, master long-distance jumps, and showcase your unique jumping style. Earn points for creativity and enthusiasm.</th>
-                <th>Evening: Relax with a soothing puddle-side spa experience. Indulge in mud masks and puddle-inspired treatments to rejuvenate and refresh.</th>
+                <th>Morning: Step into the cubicle, greeted by the luxurious ergonomic chair that adjusts to your every move. The desk, equipped with a faux wood finish, exudes an air of sophistication that will make you feel like you're working in the corner office of a skyscraper.</th>
+                <th>Afternoon: Engage in a virtual tour of the neighboring cubicles, each designed with unique themes and features. From the "Zen Oasis" with a built-in meditation corner to the "Tech Marvel" with cutting-edge gadgets, you'll be inspired by the creativity of your cubicle neighbors.</th>
+                <th>Evening: Sit alone in your cubicle till the next day.</th>
             </tr>
             <tr class="itinerary-table_info">
                 <th>3</th>
-                <th>Morning: Take a hot air balloon ride over the Puddle Paradise Resort. Marvel at the landscape dotted with shimmering puddles as you enjoy breathtaking views from above.</th>
-                <th>Afternoon: Attend a puddle art workshop. Create your own masterpiece by incorporating puddle splashes into a canvas. Embrace the spontaneous and colorful world of puddle art.</th>
-                <th>Evening: Join a puddle-themed feast. Indulge in delicious dishes inspired by water, rain, and puddles. Enjoy the culinary delights while surrounded by the enchanting ambiance of illuminated puddles.</th>
+                <th>Morning: Morning: Experience the "SmartDesk 3000," a desk with an integrated AI assistant that anticipates your needs. Need coffee? The desk senses it. Forgotten your meeting schedule? The AI has it covered, ensuring a seamless and stress-free workday.</th>
+                <th>Afternoon: Attend a cubicle networking event, where colleagues gather to share stories, insights, and maybe even some snacks. Engage in casual conversations with fellow cubicle enthusiasts and discover the hidden talents of your office mates.</th>
+                <th>Evening: Get back to work.</th>
             </tr>
             <tr class="itinerary-table_info">
                 <th>4</th>
-                <th>Morning: Challenge yourself with extreme puddle jumping. Head to the resort's puddle cliffs and experience the thrill of jumping from different heights into giant puddles. Safety is a priority, but the excitement is guaranteed.</th>
-                <th>Afternoon: Participate in a puddle photography contest. Capture the most dynamic and picturesque puddle jumps, splashes, and reflections. Share your photos with fellow puddle enthusiasts for a chance to win exciting prizes.</th>
-                <th>Evening: Attend the Puddle Carnival, a lively celebration with games, rides, and entertainment inspired by the joy of puddle jumping. Conclude the night with a grand fireworks display reflecting off the resort's largest puddle.</th>
+                <th>Morning: Immerse yourself in the "Infinite Wall of Productivity," a virtual reality display that transforms your cubicle walls into panoramic landscapes. Choose from serene beaches, bustling cityscapes, or even the view from the International Space Station, providing the perfect backdrop to boost your creativity.</th>
+                <th>Afternoon: Unwind in the "Ultimate Breakroom," a communal space filled with bean bags, foosball tables, and a never-ending supply of snacks. Take a break and recharge in this vibrant oasis that encourages collaboration and relaxation.</th>
+                <th>Evening: You spent the whole afternoon in the breakroom... The boss is mad, work overtime</th>
             </tr>
             <tr class="itinerary-table_info">
                 <th>5</th>
-                <th>Morning: Bid farewell to Puddle Paradise with a group puddle jump, creating a spectacular and memorable splash finale. Receive a certificate of achievement and a Puddle Jumping Champion medal as a memento of your exhilarating puddle adventure.</th>
+                <th>Morning: Conclude your epic cubicle adventure with a farewell gathering. Exchange contact information with your newfound cubicle friends and promise to stay in touch. As you exit the Ultimate Cubicle Haven, reflect on the extraordinary experience of working in a cubicle that surpassed all expectations.</th>
         </table>
 
 
